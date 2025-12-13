f10e4e3d3c7c7493b2d8cdf0d7b0864d4b435b82
Marti Oliver <marti.oliver.lopez@estudiant.enti.cat>
2025-12-13 17:29:27 +0100

    Actualització del fitxer de configuration per a una millor llegibilitat de les constants. Ha estat afegit el notch, agrupacio, i mes fitxers

--- configuration.py
+++ configuration.py
@@ -1,5 +1,13 @@
 #creació de constants que s'utilitzaran de manera recurrent
+#alfabet del cablejat
 ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#noms dels fitxers dels rotors
 ROTOR1 = "rotor1.txt"
 ROTOR2 = "rotor2.txt"
 ROTOR3 = "rotor3.txt"
+#noms dels fitxers de text
+TEXT_ORIGINAL = "missatge.txt"
+TEXT_XIFRAT = "xifrat.txt"
+TEXT_DESXIFRAT = "desxifrat.txt"
+NOTCH_INICIAL = "Z" #aquest sera el notch en cas de no ser especificat per l'usuari
+AGRUPACIÓ = 5 #grup de lletres amb el format que treballa ENIGMA
